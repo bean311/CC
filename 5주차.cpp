e1a891f3fd25dbc7aaa1089959bcffcbe9844ea1
bean311 <bean5667@naver.com>
2024-08-06 21:01:21 +0900

    Add files via upload

---
+++ 5주차.cpp
@@ -0,0 +1,149 @@
+#include<stdio.h>
+int main(){
+	
+/*	
+	int num;
+	int a=1;
+	
+	printf("num : ");
+	scanf("%d", &num);
+	
+	while(a<=num){
+		if(num%a==0){
+			printf("%d ", a);
+		}
+			a++;
+		}
+	}
+	*/
+
+/*
+	int even=0, odd=0;
+	
+	for(int i=1;i<=10;i++){
+		if(i%2==0){
+			even+=i;
+		}
+		else{
+			odd+=i;
+		}
+	}
+	printf("sum_even : %d\n", even);
+	printf("sum_odd : %d", odd);
+*/
+
+/*
+	for(int j=1;j<=6;j++){
+		printf("%d g ",j);
+		for(int i=1;i<=7;i++){
+			printf("%d ",i);
+		}
+		printf("\n");
+	}
+*/
+
+/*
+	int j=6;
+	
+	while(j>=1){
+		printf("%dg ",j);
+		j--;
+		int i=1;
+		while(i<=7){
+			printf("%d ",i);
+			i++;
+		}
+		printf("\n");
+	}
+*/
+
+/*
+	int i=1;
+	do{
+		printf("%dg ",i);
+	
+		int j=7;
+		do{
+			printf("%d ",j);
+			j--;
+		}while(j>=1);
+		printf("\n");
+		i++;
+	}while(i<=6);
+*/
+
+/*	
+	for(int i=2;i<=9;i++){
+		printf("<<%ddan>>\n",i);
+		for(int j=1;j<=9;j++){
+			printf("%d X %d = %d\n",i,j,i*j);
+		}	
+	}
+*/
+
+/*	
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=i;j++){
+			printf("%d ",j);
+		}
+	printf("\n");
+	}
+*/
+
+/*
+	for(int i=5;i>=1;i--){
+		for(int j=1;j<=i;j++){
+			printf("%d ",j);
+		}
+	printf("\n");
+	}
+*/
+
+/*
+	for(int i=5;i>=1;i--){
+		for(int j=i;j<=5;j++){
+			printf("%d ",j);
+		}
+	printf("\n");
+	}
+*/
+
+/*	
+	int max=0,cnt=0,best;
+	for(int i=1;i<=100;i++){
+		printf("%d : ",i);
+		cnt=0;
+		for(int j=1;j<=i;j++){
+			
+			if(i%j==0){
+				printf("%d ",j);
+				cnt++;
+			}
+		if(cnt>max){
+			max=cnt;
+			best = i;
+		}
+		}
+	printf("\n ");
+	}  
+	printf("many num : %d ",best);
+	*/
+
+/*
+	//baekjun 2438
+	int n;
+	
+	printf("n : ");
+	scanf("%d",&n);
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			printf("*");
+		}
+	printf("\n");	
+	}
+*/
+	
+//baekjun 2439	
+	
+}
